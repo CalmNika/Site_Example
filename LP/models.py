13996ceae487e5ffca6013c7ed2d578d4dd6b388
CalmNika <n.s.kalmykova@gmail.com>
2023-09-04 16:39:45 +0500

    HW_9_

--- LP/models.py
+++ LP/models.py
@@ -4,4 +4,13 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    список = models.TextField()
 
+book = Book(title="Примерная книга", author="Имя автора", publication_year=2022, список="Одиночная запись")
+book.save()
+
+data = ["Запись 1", "Запись 2", "Запись 3"]
+
+for item in data:
+    book = Book(title="Примерная книга", author="Имя автора", publication_year=2022, список=item)
+    book.save()
